0fb94750754a0dc4a4bd7e9e7ab29fdded0779f6
RonenMars <ronen@bmedia.co.il>
2018-09-03 18:22:10 +0300

    new

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,13 +1,23 @@
 import {Component} from '@angular/core';
+import { CoursesService } from './course/courses.service';
 
 @Component({
     selector: 'courses',
-    template: `<h2>ourCourses</h2>
+    template: `<h2 id="123">ourCourses</h2>
                 <ul *ngFor="let course of courses">
                     <li>{{course}}</li>
-                </ul>`
+                </ul>
+                <button class="btn btn-primary" [class.active]="active">Save</button>
+                <button [style.backgroundColor]="active ? 'green' : 'red' ">Save</button>
+                
+                `
+
 })
 
 export class CoursesComponent{
     courses;
+    active = true;
+    constructor(service: CoursesService){
+        this.courses = service.getCourses();
+    }
 }
